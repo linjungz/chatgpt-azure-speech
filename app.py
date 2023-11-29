e35a2a6dc93e5d266c5724ba6fb7b4faaa3c9511
Randy <linjrandy@gmail.com>
2023-11-29 22:19:33 +0800

    code clear

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from dotenv import load_dotenv
 
 import streamlit as st
 from streamlit_lottie import st_lottie
-import requests
+import json
 
 # Load environment variables from .env file
 load_dotenv()
 
 openai.api_type = "azure"
-openai.api_base = "https://junlin-gpt4.openai.azure.com/"
-openai.api_version = "2023-07-01-preview"
+openai.api_base = os.getenv("OPENAI_API_ENDPOINT")
+openai.api_version = os.getenv("OPENAI_API_VERSION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
@@ -38,7 +38,7 @@ def ask_and_reply(prompt, message_box):
     ]
 
     completion = openai.ChatCompletion.create(
-        engine="gpt-4-1106",
+        engine=os.getenv("OPENAI_DEPLOYMENT"),
         messages = message_text,
         temperature=0.7,
         max_tokens=800,
@@ -49,7 +49,7 @@ def ask_and_reply(prompt, message_box):
     )
 
     print("OpenAI Request sent. Now waiting for response...")
-    st.session_state.is_speaking = True
+
 
     collected_messages = []
     for chunk in completion:
@@ -68,15 +68,8 @@ def ask_and_reply(prompt, message_box):
                         message_box.write(f'ChatGPT: {text}')
                         result = speech_synthesizer.speak_text_async(text).get()
                         collected_messages.clear()
-        if st.session_state.is_speaking == False:
-            break
-
-def stop_speaking():
-    st.session_state.is_speaking = False
 
 def record_voice():
-    st.session_state.is_recording = True
-    print(st.session_state.is_recording)
     # Get audio from the microphone and then send it to the TTS service.
     speech_recognition_result = speech_recognizer.recognize_once_async().get()
 
@@ -84,7 +77,6 @@ def record_voice():
         print("Recognized speech: {}".format(speech_recognition_result.text))
         st.session_state.prompt_text = speech_recognition_result.text
         st.session_state.voice_recognized = True
-        st.session_state.is_recording = False
 
     elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
         print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
@@ -99,26 +91,23 @@ def record_voice():
 
 LOTTIE_URL = 'https://assets6.lottiefiles.com/packages/lf20_6e0qqtpa.json'
 
-# Create the animation
-def load_lottie(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return
-    return r.json()
+# Function to load JSON from file
+def load_json_from_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+# Load the Lottie animation from JSON file
+lottie_anim = load_json_from_file('./lottie.json')
+
 
 # Initialize session state
-if "is_recording" not in st.session_state:
-    st.session_state.is_recording = False
 if "prompt_text" not in st.session_state:
     st.session_state.prompt_text = None
-if "chat_text" not in st.session_state:
-    st.session_state.chat_text = None
 if "voice_recognized" not in st.session_state:
     st.session_state.voice_recognized = False
-if "is_speaking" not in st.session_state:
-    st.session_state.is_speaking = False
 
-lottie_anim = load_lottie(LOTTIE_URL)
+lottie_anim = load_json_from_file('./lottie.json')
 
 st.set_page_config(page_title="ChatGPT Voice", page_icon='', layout='centered')
 
@@ -131,14 +120,8 @@ with st.container():
         st.write('Press Record to start recording your voice.')
 
         rec_button = st.button(
-            label="Record :microphone:", type='primary',
-            on_click=record_voice,
-            disabled=st.session_state.is_recording)
-        
-        stopo_button = st.button(
-            label="Stop", type='primary',
-            on_click=stop_speaking,
-            disabled=not st.session_state.is_speaking)
+            label="Record", type='primary',
+            on_click=record_voice)
 
         message_box = st.empty()
         if st.session_state.voice_recognized:
